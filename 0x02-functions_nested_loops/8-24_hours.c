85397800471f7a1257de3615b74c357165cc7eea
Ngwanatuka <ngwanatuka@gmail.com>
2022-09-13 20:32:40 +0200

    intitial commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int hr;
+	int min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			putchar((hr / 10) + '0');
+			putchar((hr % 10) + '0');
+			putchar(':');
+			putchar((min / 10) + '0');
+			putchar((min % 10) + '0');
+			putchar('\n');
+		}
+	}
+}
