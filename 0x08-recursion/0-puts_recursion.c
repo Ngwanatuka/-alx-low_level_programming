095bcdf22e2f44323b2b88ad06555012dd59fa7d
Ngwantuka <ngwanatuka@gmail.com>
2022-09-28 20:44:49 +0200

    initial commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,19 @@
  */
 void _puts_recursion(char *s)
 {
-	printf("%s\n", s);
+	if (*s == '\0')
+		putchar('\n');
+	else
+	{
+		putchar(s[0]);
+		_puts_recursion(s +1);
+	}
 }
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(void)
 {
 	_puts_recursion("Puts with recursion");
