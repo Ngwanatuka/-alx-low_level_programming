f72d638b7af4086862e3119a06e618f5ec19a142
Ngwantuka <ngwanatuka@gmail.com>
2022-09-28 21:11:07 +0200

    ....

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -15,13 +15,3 @@ void _puts_recursion(char *s)
 		_puts_recursion(s +1);
 	}
 }
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
-{
-	_puts_recursion("Puts with recursion");
-	return (0);
-}
