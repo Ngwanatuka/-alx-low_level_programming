667139b1540827f73cc6b2a1b03e1ae5e65e1a77
Ngwantuka <ngwanatuka@gmail.com>
2022-09-28 23:19:17 +0200

    adding a second function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,32 @@
 #include "main.h"
 
 /**
-* _pow_recursion - function that returns the
-* value of x raised to the power of y
-*@x: the base
-*@y: the exponent
-*Return: the value of x raised to the power of y
+* _sqrt_recursion - returns the natural square root of a number
+* @n: number to be used
+*
+* Return: the square root of n
 */
-int _pow_recursion(int x, int y)
+
+int _sqrt_recursion(int n)
 {
-		if (y < 0)
-			return (-1);
-		else if (y == 0)
-			return (1);
-		return (_pow_recursion(x, y - 1) * x);
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt(0, n));
+}
+
+/**
+ * _sqrt - returns the square root of a number
+ * @n: test number
+ * @x: squared number
+ *
+ * Return: the square root of n
+ */
+
+int _sqrt(int n, int x)
+{
+	if (n > x / 2)
+		return (-1);
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
 }
