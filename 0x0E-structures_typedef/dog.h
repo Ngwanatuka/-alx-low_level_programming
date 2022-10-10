977ab532c662845bd5ba36c650c81835c88e132a
Ngwantuka <ngwanatuka@gmail.com>
2022-10-10 12:48:48 +0200

    first three programs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+/**
+ * struc dog - a new type of data
+ * @name: the name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dof_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
