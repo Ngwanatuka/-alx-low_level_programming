09a8141e41d2623c530e259428707bcf31d4bbe0
Ngwantuka <ngwanatuka@gmail.com>
2023-01-12 19:24:41 +0200

    fixing requirements

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -7,30 +7,47 @@
  * @value: The value associated with key.
  *
  * Return: Upon failure - 0.
- * otherwise -1.
+ *         Otherwise - 1.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	if (!ht || !*key || !value)
-		return (0);
-	unsigned long int index = key_index(key, ht->size);
-	hash_node_t *current;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-	for (current = ht->array[index]; current; current = current->next)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
-		if (!strcmp(current->key, key))
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			free(current->value);
-			current->value = strdup(valuee);
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
 			return (1);
 		}
 	}
-	current = malloc(sizeof(hash_nide_t));
-	if (!currnet)
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
 		return (0);
-	current->key = strdup(key);
-	current->value = strdup(value);
-	current->next = ht->array[index];
-	ht->array[index] = current;
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
 	return (1);
 }
