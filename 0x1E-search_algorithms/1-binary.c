694b216bda13bb7d2a5da47a5f1384f4933cc1fd
Ngwanatuka <Ngwanatuka@gmail.com>
2023-05-10 13:25:02 +0200

    fixing code

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -21,7 +21,7 @@ int binary_search(int *array, size_t size, int value)
 			printf("%d, ", array[i]);
 		printf("%d\n", array[right]);
 
-		mid = left + (right - left) / 2;
+		mid = (left + left) / 2;
 		if (array[mid] == value)
 			return (mid);
 		else if (array[mid] < value)
